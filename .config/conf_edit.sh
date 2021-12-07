35262672ec8b14eb1d45aee9577855317e2ef950
Arnav Dixit <arnavdixit127@gmail.com>
2021-12-07 17:17:29 +0530

    add: add conf_edit script to edit configs through dmenu

---
+++ .config/conf_edit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script to choose config file to edit
+
+TERMINAL="konsole"
+EDITOR="vim"
+declare -A config
+
+# add config files here
+config[vim]=~/.vimrc
+config[i3]=~/.config/i3/config
+config[alacritty]=~/.config/alacritty/alacritty.yml
+config[picom]=~/.config/picom/picom.conf
+config[polybar]=~/.config/polybar/config.ini
+config[conf_edit]=~/.config/conf_edit.sh
+config[rofi]=~/.config/rofi/config.rasi
+config[konsole]=~/.config/konsolerc
+config[dunst]=~/.config/dunst/dunstrc
+
+choice=$(printf "%s\n" "${!config[@]}" | sort | rofi -dmenu -i -p "Config" -auto-select | cut -f 1)
+
+if [ -n "$choice" ]; 
+    then $TERMINAL -e $EDITOR ${config[$choice]} 
+fi
+
